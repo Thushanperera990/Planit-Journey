65252165c7c42c20926d628a5b5719638d2200ad
Yahesha <90343221+Yahesha@users.noreply.github.com>
2026-01-16 20:26:38 +0530

    delete

--- frontend/src/component/clientside/Card.jsx
+++
@@ -1,9 +0,0 @@
-const Card = ({ children }) => {
-  return (
-    <div className="p-4  bg-white shadow-md shadow-gray-300 rounded-md mb-5">
-      {children}
-    </div>
-  );
-};
-
-export default Card;
